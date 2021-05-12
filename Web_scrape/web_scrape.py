4b324103628dfab9da7d24279e872174fc5cebe3
jeffreychan <kkjeffreychan@gmail.com>
2021-05-11 22:41:47 -0400

    Able to traverse through multiple pages, still inconsistencies present

--- Web_scrape/web_scrape.py
+++ Web_scrape/web_scrape.py
@@ -6,40 +6,43 @@ driver = webdriver.Chrome(executable_path="./Drivers/chromedriver")
 
 driver.get(
     "https://academiccalendars.romcmaster.ca/content.php?catoid=41&navoid=8647")
+driver.implicitly_wait(5)
 
-# returns the number of rows within the table
-table = driver.find_elements_by_xpath(
-    '//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr')
+for i in range(1, 10):
+    # returns the number of rows within the table
+    list_rows = driver.find_elements_by_xpath(
+        '//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr')
 
-# iterate all rows and print info,
-for i in range(len(table)):
+    # iterate all rows and print info,
+    for j in range(1, len(list_rows)-1):
+        # printing units(all course stuff)
+        try:
+            open_link = driver.find_element_by_xpath(
+                f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{j}]/td[2]/a')
+            open_link.click()
 
-    # printing course name first
-    try:
-        open_link = driver.find_element_by_xpath(
-            f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{i}]/td[2]/a')
-        open_link.click()
-        driver.implicitly_wait(100)
+            units = driver.find_element_by_xpath(
+                f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{j}]/td[2]/table/tbody/tr/td/div[2]').text
+            print(units)
+        except:
+            print("no course units")
 
-        title = driver.find_element_by_xpath(
-            f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{i}]/td[2]/table/tbody/tr/td/div[2]/h3').text
-        print(title)
-    except:
-        print("There is no course name here")
+    next_page = driver.find_element_by_xpath(
+        f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[104]/td/a[{i}]')
+    next_page.click()
 
-    # printing units
-    # units = driver.find_element_by_xpath(
-    #     f'//*[@id = "table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[4]/td[2]/table/tbody/tr/td/div[2]/text()[1]').text
-    # print(units)
+# printing course name first
+# try:
+#     open_link = driver.find_element_by_xpath(
+#         f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{i}]/td[2]/a')
+#     open_link.click()
+#     driver.implicitly_wait(100)
 
-    # printing course description
-
-    # try:
-    #     description = driver.find_element_by_xpath(
-    #         f'//*[@id = "table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{i}]/td[2]/table/tbody/tr/td/div[2]/text()[2]').text
-    #     print(description)
-    # except:
-    #     print("There are no course description here")
+#     title = driver.find_element_by_xpath(
+#         f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{i}]/td[2]/table/tbody/tr/td/div[2]/h3').text
+#     print(title)
+# except:
+#     print("There is no course name here")
 
 
 # THIS WORKS FOR ONE TITLE
