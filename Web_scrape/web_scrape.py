31d2f6fad64e666972851b83b1cf4603219923dd
jeffreychan <kkjeffreychan@gmail.com>
2021-05-13 17:38:47 -0400

    instead of xpath for next page, use ccs selector

--- Web_scrape/web_scrape.py
+++ Web_scrape/web_scrape.py
@@ -8,13 +8,15 @@ driver.get(
     "https://academiccalendars.romcmaster.ca/content.php?catoid=41&navoid=8647")
 driver.implicitly_wait(5)
 
-for i in range(1, 10):
+# how many pages of courses there are
+for i in range(2, 10):
+
     # returns the number of rows within the table
     list_rows = driver.find_elements_by_xpath(
         '//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr')
 
-    # iterate all rows and print info,
-    for j in range(3, len(list_rows)-1):
+    # iterate all rows and print info, first 3 rows are empty
+    for j in range(14, len(list_rows)-1):
         # printing units(all course stuff)
         try:
             open_link = driver.find_element_by_xpath(
@@ -32,13 +34,19 @@ for i in range(1, 10):
         except:
             print("no course units")
 
-    next_page = driver.find_element_by_xpath(
-        f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[104]/td/a[{i}]')
-    next_page.click()
+    # find last row of table, then press the next page
+    last_row = driver.find_element_by_css_selector(
+        f'[aria-label = "Page {i}"]')
+    last_row.click()
 
 # instead of increment by 1 on this xpath, find the last row of the table
 
-
+# //*[@id = "table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[104]/td/a[1]
+# //*[@id = "table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[104]/td/a[2]
+# //*[@id = "table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[105]/td/a[3]
+# //*[@id = "table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[107]/td/a[4]
+# //*[@id = "table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[108]/td/a[5]
+# //*[@id = "table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[111]/td/a[6]
 # printing course name first
 # try:
 #     open_link = driver.find_element_by_xpath(
