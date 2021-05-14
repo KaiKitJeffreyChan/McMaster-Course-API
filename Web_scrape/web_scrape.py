3f390a081c165bbcf226ca7b2bb9e6962a7f4dd9
jeffreychan <kkjeffreychan@gmail.com>
2021-05-14 18:25:04 -0400

    Fixed csv formatting when writing

--- Web_scrape/web_scrape.py
+++ Web_scrape/web_scrape.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import csv
+import os
+os.remove('final.csv')
 
 driver = webdriver.Chrome(executable_path="./Drivers/chromedriver")
 
 driver.get(
     "https://academiccalendars.romcmaster.ca/content.php?catoid=41&navoid=8647")
 driver.implicitly_wait(5)
+
+# add header
+with open("final.csv", 'a', newline='') as fl:
+    thewriter = csv.writer(fl)
+    thewriter.writerow(["Course Name", "Units",
+                        "Description", "Other"])
 
 # how many pages of courses there are
 for i in range(2, 33):
@@ -17,22 +25,28 @@ for i in range(2, 33):
 
     # iterate all rows and print info, first 3 rows are empty last row also empty
     for j in range(3, len(list_rows)-1):
-        # printing units(all course stuff)
+        # printing content(all course stuff)
         try:
             open_link = driver.find_element_by_xpath(
                 f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{j}]/td[2]/a')
             open_link.click()
 
-            units = driver.find_element_by_xpath(
+            content = driver.find_element_by_xpath(
                 f'//*[@id="table_block_n2_and_content_wrapper"]/table/tbody/tr[2]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[{j}]/td[2]/table/tbody/tr/td/div[2]').text
 
-            with open('courseInfo.csv', 'a', newline='') as f:
-                thewriter = csv.writer(f)
-                thewriter.writerow([units])
+            content = content.split("\n", 3)
+            with open("final.csv", 'a', newline='') as fl:
+                thewriter = csv.writer(fl)
+                try:
+                    thewriter.writerow(
+                        [content[0], content[1], content[2], content[3]])
+                except:
+                    thewriter.writerow(
+                        [content[0], content[1], content[2], "nothing"])
 
-            print(units)
+            print(content)
         except:
-            print("no course units")
+            print("no course content")
 
     # find last row of table, then press the next page
     last_row = driver.find_element_by_css_selector(
