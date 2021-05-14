acd97dae0e89319da2afce3e78c933754cc4f614
jeffreychan <kkjeffreychan@gmail.com>
2021-05-14 17:41:09 -0400

    using intermediate csv to sort info, will edit so there is no csv intermediate

--- Web_scrape/organize.py
+++ Web_scrape/organize.py
@@ -3,15 +3,18 @@ import csv
 # Sort CSV Data
 with open("courseinfo.csv", 'r') as f:
     thereader = csv.reader(f)
+
+    with open("final.csv", 'a', newline='') as fl:
+        thewriter = csv.writer(fl)
+        thewriter.writerow(["Course Name", "Units",
+                            "Description", "Other"])
     for row in thereader:
-        # row = row.split("\n", 3)
+        row = row[0].split("\n", 3)
         print(row[0])
 
-        # with open("final.csv", 'w', newline='') as fl:
-        #     thewriter = csv.writer(fl)
-        #     thewriter.writerow(["Course Name", "Units",
-        #                         "Description", "Other"])
-        #     try:
-        #         thewriter.writerow([row[0], row[1], row[2], row[3]])
-        #     except:
-        #         thewriter.writerow([row[0], row[1], row[2]])
+        with open("final.csv", 'a', newline='') as fl:
+            thewriter = csv.writer(fl)
+            try:
+                thewriter.writerow([row[0], row[1], row[2], row[3]])
+            except:
+                thewriter.writerow([row[0], row[1], row[2], "nothing"])
