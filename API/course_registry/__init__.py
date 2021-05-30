ddd1f32c27e76b7a6ad56fa401d9f1519a7af27d
jeffreychan <kkjeffreychan@gmail.com>
2021-05-30 00:44:16 -0400

    playing with code

--- API/course_registry/__init__.py
+++ API/course_registry/__init__.py
@@ -1,18 +1,28 @@
 from flask import Flask
+from flask_restful import Api, Resource
 import markdown
 import os
 
 # instance of flask
 app = Flask(__name__)
+api = Api(app)
+
+# @app.route('/')
+# def index():
+#     """open documentation"""
+
+#     # open the readmee
+#     with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
+
+#         content = markdown_file.read()
+
+#         return markdown.markdown(content)
 
 
-@app.route('/')
-def index():
-    """open documentation"""
+class HelloWorld(Resource):
+    def get(self):
+        # must return something serializable
+        return {"data": "hello world", "message": "hoiiiii"}
 
-    # open the readmee
-    with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
 
-        content = markdown_file.read()
-
-        return markdown.markdown(content)
+api.add_resource(HelloWorld, "/helloworld")
