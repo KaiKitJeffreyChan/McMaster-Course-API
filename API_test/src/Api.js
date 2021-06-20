ff54aa9e0a79f97ea1cfed829ef26b9e45bab8fa
jeffreychan <kkjeffreychan@gmail.com>
2021-06-20 00:27:00 -0400

    cleaning up testing code

--- API_test/src/Api.js
+++ API_test/src/Api.js
@@ -1,19 +1,9 @@
 import axios from "axios";
 
 const GetCourses = () => {
-  // return axios
-  //   .get("https://randomuser.me/api")
-  //   .then(({ data }) => {
-  //     console.log(data);
-  //     return data;
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
   return axios
     .get("https://mcmaster-course-api.herokuapp.com/courses/CLASSICS 1A03")
     .then((data) => {
-      console.log(data);
       return data;
     })
     .catch((err) => {
--- API_test/src/App.js
+++ API_test/src/App.js
@@ -1,33 +1,7 @@
 import { useState, useEffect } from "react";
 import GetCourses from "./Api";
-import User from "./Components/user";
-
-const getInfo = (userInformation) => {
-  const {
-    name: { first, last },
-    picture: { large },
-  } = userInformation;
-
-  return { name: `Hello ${first} ${last}!!!`, img: large };
-};
 
 function App() {
-  const [data, setData] = useState("");
-  const [courses, setCourses] = useState("");
-  const [userInformations, setUserInformations] = useState([]);
-
-  // useEffect(() => {
-  //   GetCourses()
-  //     .then((retriveData) => {
-  //       // setData(JSON.stringify(retriveData, null, 2));
-  //       setUserInformations(retriveData.results);
-  //       console.log(userInformations);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
-
   return (
     <div className="App">
       <button
@@ -43,10 +17,6 @@ function App() {
       >
         Get
       </button>
-      {/* {userInformations.map((userInformation, idx) => {
-        const { name, img } = getInfo(userInformation);
-        return <User key="userInformation" name={name} img={img} />;
-      })} */}
     </div>
   );
 }
