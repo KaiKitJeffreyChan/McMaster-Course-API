0a3cbff30aebb30fb404ead5cea3e0ef590b4030
Jeffrey Chan <77026758+KaiKitJeffreyChan@users.noreply.github.com>
2021-08-02 18:07:35 -0400

    Update run.py

--- API/run.py
+++ API/run.py
@@ -1,6 +1,6 @@
 from course_registry import app
 import os
-# port 80 (server side) to 5000 (client side)????
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
